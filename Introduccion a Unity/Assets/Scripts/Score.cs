0c371f856f3689136dfc862b70c64c1586a8e26c
ZaruWright <samuel_gse@hotmail.com>
2015-04-25 15:50:47 +0200

    player parece que no tiene bien definido las colisiones

---
+++ Introduccion a Unity/Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Score : MonoBehaviour {
+
+	public GUISkin m_skin;
+	public Vector2 m_offset;
+	public Vector2 m_size;
+	private int m_score = 0;
+	
+	void OnGUI()
+	{
+		//SCORE
+		
+		//GUI.skin = m_skin;
+		GUI.TextArea(new Rect(Screen.width * m_offset.x,Screen.height * m_offset.y,  Screen.width* m_size.x, Screen.height * m_size.y), "Score: " + m_score, m_skin.GetStyle("textarea"));
+	
+	}
+	
+	void AddScore(int f)
+	{
+		m_score += f;
+	}
+	
+	void SetScore(int f)
+	{
+		m_score = f;
+	}
+
+}
